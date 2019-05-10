4a7fd3cf46096258ee7efacd7cae5844bb7a722c
root <JCrommen@cincom.com>
2019-05-10 18:50:41 +0200

    Initial application generated by JHipster-6.0.0

---
+++ src/main/java/fr/chocolateroad/gateway/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,20 @@
+package fr.chocolateroad.gateway.security;
+
+import fr.chocolateroad.gateway.config.Constants;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of {@link AuditorAware} based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of(SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM_ACCOUNT));
+    }
+}
---
+++ src/main/java/fr/chocolateroad/gateway/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package fr.chocolateroad.gateway.web.rest.vm;
